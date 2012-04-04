60bc9d495eec68a662d00ed0ae685aab0d48b234
Willem Mulder <willemmulder@gmail.com>
2012-04-04 11:53:51 +0200

    Slimmed down matrix libraries to 1 compact file

--- js/sylvester/exports.js
+++
@@ -1,9 +0,0 @@
-if (typeof require === 'function' && typeof module === 'object') {
-  module.exports = Sylvester;
-} else {
-  this.Line    = Sylvester.Line;
-  this.Matrix  = Sylvester.Matrix;
-  this.Plane   = Sylvester.Plane;
-  this.Polygon = Sylvester.Polygon;
-  this.Vector  = Sylvester.Vector;
-}
